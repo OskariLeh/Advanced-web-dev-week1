af5b98c97464a87f621b5e23b54b6f7d0fc373a3
Oskari <oskari.m.lehtonen@student.lut.fi>
2023-11-05 01:15:49 +0200

    Week 1 initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+documentBody = document.querySelector("body")
+
+const createWikiItems =  () => {
+    wikiItem = document.createElement("div")
+    wikiItem.class
+    wikiHeader = document.createElement("h1")
+    wikiHeader.innerText = "Breed X"
+    wikiItem.appendChild(wikiHeader)
+    documentBody.appendChild(wikiItem)
+}
+
+createWikiItems()
